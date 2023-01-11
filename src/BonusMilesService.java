2b31adf6178ae3d0d0fcf35fcd33216034dc8d5f
LuNTIK969 <nasa.969@gmail.com>
2023-01-11 18:31:18 +0300

    code UPD

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        int miles = price / 20;
-        return miles;
+    public int calculate(int cost) {
+        return cost / 20;
     }
 }
